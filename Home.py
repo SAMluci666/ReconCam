10c008f27f124da6bf50f54b84b30a1bb9d6b0bb
Samar Singla <84288735+SAMluci666@users.noreply.github.com>
2024-09-15 01:27:29 +0530

    Add base application

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title='Attendance System',layout='wide')
+
+st.header('Smart Student Monitoring and Attendance System')
+
+with st.spinner("Developed by Harsh Vaidya"):
+    import face_rec
+    
+st.success('Welcome to Our System')
+st.success('All set! Please enter your details in registration form if you are new user!')
+st.success('If you already enrolled in this system then please go in Real Time Prediction and start the cam!')
+
+
