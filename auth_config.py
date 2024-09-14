5e5e90115de7eff1d9f757ba6d50aae1a8ace5c8
Samar Singla <84288735+SAMluci666@users.noreply.github.com>
2024-09-14 22:56:21 +0530

    Add files via upload

---
+++ auth_config.py
@@ -0,0 +1,6 @@
+auth0_config = {
+    'client_id': 'Zlt4jv9u2N0yZwunhzUPsWLaFhPVmRhr',
+    'client_secret': 'R7LTnipUTLQ2Fem9QR39eRwVySXFsMaLDPCyUD_sKkMtqcL-bbeVC_HGsgCgSlju',
+    'domain': 'dev-ovhec64kn1hlh63s.us.auth0.com',
+    'redirect_uri': 'https://13.60.40.105/'
+}
